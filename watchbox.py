29634b7aecd809000b578f7c666484991b096fed
MaceroniMan <89584069+MaceroniMan@users.noreply.github.com>
2022-06-19 08:39:07 -0500

    Overhaul of join technique
    Now when a client tries to connect to the server they must use the 'register' command to connect, otherwise they are rejected.
    Log messages are now sent to the server and the server saves them in 'client.watchbox.log'
    All messages are turned into JSON on both ends which will allow for future cross-language support as most major languages support JSON.
    Re-did the server onMessage function, now returns a internal object for neater code calling and a built-in 'reply' function.
    The 'send' function is now a internal call because the onMessage provides that functionality for it.
    Added extra logging messages to the flask logging.

--- watchbox.py
+++ watchbox.py
@@ -3,9 +3,9 @@ from flask import request, make_response
 from datetime import datetime
 import threading
 import urllib.parse
-import uuid, time, sys
+import uuid, time, sys, json
 
-minijs = """var __watchboxvars__={connected:!0,joingroups:[],inited:!1};async function __watchboxdeamon__(o,n,t){let c=!0,e=o;for(;;)for(item in await watchbox._sleep(e),__watchboxvars__.joingroups){let i=__watchboxvars__.joingroups[item][0],a=__watchboxvars__.joingroups[item][1];new Promise(function(a,s){$.post("/watchbox.get.wb",{group:i},function(n){c||(c=!0,e=o,console.log("[watchbox] connected"),t()),a("wb00"==n?"":"wb11"==n?"join":n)}).fail(function(){c&&(e=1e3,c=!1,console.log("[watchbox] disconnected"),n())})}).then(async function(o){"join"==o?await watchbox._join(i):""==o||a(o)},function(o){})}}const watchbox={_sleep:async function(o){return new Promise(n=>setTimeout(n,o))},init:function(o=100,n=function(){},t=function(){}){__watchboxdeamon__(o,n,t),__watchboxvars__.inited=!0},_join:async function(o){$.post("/watchbox.join.wb",{group:o},function(o){console.log("[watchbox] join response: "+o)})},join:function(o,n,t=function(){}){__watchboxvars__.inited?(__watchboxvars__.joingroups.push([o,n]),t()):console.error("[watchbox] watchbox not inited, use 'watchbox.init'")},publish:function(o,n){__watchboxvars__.inited?$.post("/watchbox.brdcst.wb",{group:n,text:encodeURIComponent(o)},function(o){console.log("[watchbox] message response: "+o)}):console.error("[watchbox] watchbox not inited, use 'watchbox.init'")},send:function(o){__watchboxvars__.inited?$.post("/watchbox.server.wb",{text:encodeURIComponent(o)},function(o){console.log("[watchbox] message response: "+o)}):console.error("[watchbox] watchbox not inited, use 'watchbox.init'")}};"""
+minijs = """var __watchboxvars__={joingroups:[],inited:!1,logginglevel:3,registered:!1,dtime:0,serverlogs:!0,reconnect:function(){},disconnect:function(){}};async function __watchboxdeamon__(){for(;;)if(await watchbox._sleep_(__watchboxvars__.dtime),__watchboxvars__.registered)for(item in __watchboxvars__.joingroups){let o=__watchboxvars__.joingroups[item][0],_=__watchboxvars__.joingroups[item][1];new Promise(function(_,t){$.post("/watchbox.get.wb",{group:o},function(o){_("wb00"==o?"":"wb11"==o?"join":"wb41"==o?"register":JSON.parse(o))}).fail(function(){__watchboxvars__.registered&&(__watchboxvars__.registered=!1,watchbox._log_("disconnected",2),__watchboxvars__.disconnect())})}).then(async function(t){"join"==t?await watchbox._join_(o):""==t||("register"==t?(__watchboxvars__.registered=!1,watchbox._log_("disconnected",2),__watchboxvars__.disconnect()):_(t))},function(o){})}else null!=__watchboxvars__.registered&&watchbox._register_()}const watchbox={_sleep_:async function(o){return new Promise(_=>setTimeout(_,o))},_log_:function(o,_){_<=__watchboxvars__.logginglevel&&(1==_?console.error("[watchbox] "+o):console.log("[watchbox] [level "+_+"] "+o),__watchboxvars__.serverlogs&&watchbox.send(["_log_","internals",o],!0))},_register_:async function(){for(__watchboxvars__.registered=null;!__watchboxvars__.registered;){await watchbox._sleep_(1e3);var o=new Promise(function(o,_){$.post("/watchbox.register.wb",{},function(_){"wb42"==_?o(!0):"wb41"==_&&o(!1)}).fail(function(){o(!1)})});await o.then(function(o){o?(watchbox._log_("connected",2),__watchboxvars__.reconnect(),__watchboxvars__.registered=!0):__watchboxvars__.registered=null},function(o){})}},_join_:async function(o){$.post("/watchbox.join.wb",{group:o},function(_){watchbox._log_("join response: "+_,2),watchbox._log_("from group: "+o,3)})},init:function(o=100,_=3,t=function(){},e=function(){}){__watchboxdeamon__(),__watchboxvars__.dtime=o,__watchboxvars__.disconnect=t,__watchboxvars__.reconnect=e,__watchboxvars__.logginglevel=_,__watchboxvars__.inited=!0},join:function(o,_,t=function(){}){__watchboxvars__.inited?(__watchboxvars__.joingroups.push([o,_]),t()):watchbox._log_("watchbox not inited, use 'watchbox.init'",1)},publish:function(o,_){__watchboxvars__.inited?"server"==_?watchbox._log_("cannot send messages to locked group",1):$.post("/watchbox.brdcst.wb",{group:_,text:encodeURIComponent(JSON.stringify(o))},function(o){watchbox._log_("message response: "+o,3)}):watchbox._log_("watchbox not inited, use 'watchbox.init'",1)},send:function(o,_){__watchboxvars__.inited?$.post("/watchbox.server.wb",{text:encodeURIComponent(JSON.stringify(o))},function(o){_?"wb24"==o&&(__watchboxvars__.serverlogs=!1):watchbox._log_("message response: "+o,3)}):watchbox._log_("watchbox not inited, use 'watchbox.init'",1)}};"""
 
 class server(object):
   def __init__(self, app):
@@ -15,10 +15,12 @@ class server(object):
     self.__servermsg = None
     self.__servertimeout = None
     self.__serverjoin = None
+    self.__serverregister = None
 
     self.logginglevel = 3
     self.timeout = 10
     self.listlimit = 50
+    self.clientlogging = True
 
     self._app = app
 
@@ -37,9 +39,14 @@ class server(object):
   def __log(self, message, level):
     if self.logginglevel >= level:
       now = datetime.now()
-      print("watchbox : " + now.strftime("%H:%M:%S") + " : " + message)
+      print("watchbox : level " + str(level) + " : " + now.strftime("%Y-%m-%d %H:%M:%S %p") + " : " + message)
     else:
       pass
+
+  def __clientlog(self, application, text, uid):
+    with open("client.watchbox.log", "a") as file:
+      now = datetime.now()
+      file.write("watchbox : " + str(application) + " : " + now.strftime("%Y-%m-%d %H:%M:%S %p") + " : " + uid + " : " + text + "\n")
   
   def __checkcookie(self, rqst):
     clientid = rqst.cookies.get('__wb.client')
@@ -64,8 +71,8 @@ class server(object):
       for uid in copy:
         if time.time()-self.__clients[uid]["tm"] > self.timeout:
           self.__remove(uid)
-          self.__log("client timed out", 1)
-          if not self.__servertimeout == None:
+          self.__log("client timed out", 2)
+          if self.__servertimeout != None:
             self.__servertimeout(uid)
       time.sleep(.5)
 
@@ -78,84 +85,102 @@ class server(object):
     
   def __process(self, rqst, mtype):
     cokie = self.__checkcookie(rqst)
-    if cokie != False:
+
+    if mtype == "register" and cokie == False:
+      pass
+    elif cokie == False:
+      res = make_response("wb41")
+      return res
+    else:
       self.__clients[cokie]["tm"] = time.time()
+
     if mtype == "join":
       grup = rqst.form['group']
-      self.__log("client joined server '" + grup + "'", 1)
+      self.__log("client joined group '" + grup + "'", 2)
       res = make_response("wb31")
-      if cokie == False:
-        uid = uuid.uuid4().hex
-        res.set_cookie('__wb.client', uid)
-        self.__clients[uid] = {"tm":time.time()}
-        self.__messages[uid] = {grup:[]}
-      else:
-        uid = cokie
-        self.__messages[cokie][grup] = []
+      self.__messages[cokie][grup] = []
       if self.__serverjoin != None: # alert the server of a join
-        self.__serverjoin(uid)
+        self.__serverjoin(cokie)
       return res
     elif mtype == "brdcst":
       grup = rqst.form['group']
-      self.__log("client broadcast message to server '" + grup + "'", 3)
+      self.__log("client broadcast message to group '" + grup + "'", 3)
       text = urllib.parse.unquote(rqst.form['text'])
       res = make_response("wb21")
-      if cokie == False:
-        uid = uuid.uuid4().hex
-        res.set_cookie('__wb.client', uid)
-        self.__clients[uid] = {"tm":time.time()}
-        self.__messages[uid] = {}
-      else:
-        pass
       self.__publish(text, grup)
       return res
     elif mtype == "server":
-      text = urllib.parse.unquote(rqst.form['text'])
-      if self.__servermsg == None:
-        self.__log("tried to send message to server, but server was not watching", 1)
-        res = make_response("wb23")
-        return res
-      else:
-        res = make_response("wb21")
-        if cokie == False:
-          uid = uuid.uuid4().hex
-          res.set_cookie('__wb.client', uid)
-          self.__clients[uid] = {"tm":time.time()}
-          self.__messages[uid] = {}
+      item = json.loads(urllib.parse.unquote(rqst.form['text']))
+      if type(item) == list:
+        if len(item) == 3:
+          if item[0] == "_log_":
+            if self.clientlogging:
+              res = make_response("wb22")
+              self.__clientlog(item[1], item[2], cokie)
+              return res
+            else:
+              res = make_response("wb24")
+              return res
+          else:
+            pass
         else:
-          uid = cokie
-        self.__servermsg(self, text, uid)
-        return res
+          pass
+      else:
+        if self.__servermsg == None:
+          self.__log("client tried to send message to server, but server was not watching", 1)
+          res = make_response("wb23")
+          return res
+        else:
+          res = make_response("wb22")
+          self.__servermsg(self, item, cokie)
+          return res
     elif mtype == "get":
       grup = rqst.form['group']
       self.__log("client got messages from server '" + grup + "'", 3)
-      setcokie = False
-      if cokie == False:
-        uid = uuid.uuid4().hex
-        setcokie = True
-        self.__clients[uid] = {"tm":time.time()}
-        self.__messages[uid] = {}
-      else:
-        uid = cokie
 
-      if grup in self.__messages[uid]:
-        if len(self.__messages[uid][grup]) != 0:
-          res = make_response(str(self.__messages[uid][grup].pop(0)))
+      if grup in self.__messages[cokie]:
+        if len(self.__messages[cokie][grup]) != 0:
+          res = make_response(str(self.__messages[cokie][grup].pop(0)))
         else:
           res = make_response("wb00")
       else:
         res = make_response("wb11")
       
-      if setcokie:
-        res.set_cookie('__wb.client', uid)
-      
       return res
+    elif mtype == "register":
+      res = make_response("wb42")
+
+      if cokie == False:
+        uid = uuid.uuid4().hex
+        res.set_cookie('__wb.client', uid)
+        self.__clients[uid] = {"tm":time.time()}
+        self.__messages[uid] = {}
+
+        if self.__serverregister != None: # alert the server of a register
+          self.__serverregister(uid)
+
+        self.__log("client registered to server", 2)
+
+      return res
+
+  def _send(self, jsobject, uid, force=False):
+    try:
+      if force:
+        self.__messages[uid]["server"].append(json.dumps(jsobject))
+      else:
+        if len(self.__messages[uid]["server"]) >= self.listlimit:
+          self.__log("list limit reached on client, message not sent", 2)
+        else:
+          self.__messages[uid]["server"].append(json.dumps(jsobject))
+    except KeyError:
+      self.__log("client not listing to 'server' group, message not sent", 2)
+      
 
   def onMessage(self):
     def decorator(function):
       def magic(selfs, msg, uid):
-        selfs.__log("message sent to server", 2)
-        function({"msg": msg, "client": uid})
+        selfs.__log("message sent to server", 3)
+        function(_message_object(msg, uid, self))
       self.__servermsg = magic
     return decorator
 
@@ -166,6 +191,13 @@ class server(object):
       self.__serverjoin = magic
     return decorator
 
+  def onRegister(self):
+    def decorator(function):
+      def magic(uid):
+        function(uid)
+      self.__serverregister = magic
+    return decorator
+
   def onTimeout(self):
     def decorator(function):
       def magic(uid):
@@ -173,17 +205,29 @@ class server(object):
       self.__servertimeout = magic
     return decorator
   
-  def publish(self, text, group):
-    self.__publish(text, group)
+  def publish(self, jsobject, group):
+    self.__publish(json.dumps(jsobject), group)
+
 
 class _watchbox_stream(object):
   def write(text):
-    if "watchbox.brdcst.wb" in text or "watchbox.server.wb" in text or "watchbox.join.wb" in text or "watchbox.get.wb" in text or "watchbox.file.wb" in text:
+    if "watchbox.brdcst.wb" in text or "watchbox.server.wb" in text or "watchbox.join.wb" in text or "watchbox.get.wb" in text or "watchbox.file.wb" in text or "watchbox.register.wb" in text:
       pass
+    elif text.startswith(" * Running on http"):
+      indexoftext = text.find("(Press CTRL+C to quit)")
+      sys.stdout.write(text[:indexoftext] + "with WatchBox (Press CTRL+C twice to quit)\n")
     else:
       sys.stdout.write(text)
   def flush():
     sys.stdout.flush()
+
+class _message_object(object):
+  def __init__(self, jsonmessage, clientuuid, refself):
+    self.msg = jsonmessage # dont need to json.loads here because already done in the 'watchbox.server.wb' function
+    self.client = clientuuid
+    self.__refself = refself
+  def reply(self, jsobject, force=False):
+    self.__refself._send(jsobject, self.client, force)
 
 def run(watcher, **kwargs):
   dictConfig({
@@ -207,6 +251,9 @@ def run(watcher, **kwargs):
 # wb00: Null
 # wb11: Not in group
 # wb21: Message sent
+# wb22: Server reveived message
 # wb23: Server not listining
+# wb24: Server rejected message
 # wb31: Joined server
-# wb41: User not found
+# wb41: Not registered
+# wb42: Register success
